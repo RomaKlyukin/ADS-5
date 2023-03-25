57afcff83ee34ec06d65cbdee071f4b531862f50
RomaKlyukin <125215143+RomaKlyukin@users.noreply.github.com>
2023-03-25 10:46:14 +0300

    b108

--- include/tstack.h
+++ include/tstack.h
@@ -1,14 +1,17 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
-
+const int size = 100;
 template<typename T, const int size>
 class TStack {
  private:
-  T arr[size];
+  T* arr;
   int top;
  public:
-  TStack():top(-1) { }
+  TStack() {
+    arr = new T[size];
+    top = -1;
+  }
   T get() const {
     return arr[top];
   }
@@ -25,6 +28,9 @@ class TStack {
   void push(T item) {
     if (top < size-1)
       arr[++top] = item;
+  }
+  ~TStack(){
+    delete[] arr;
   }
 };
 
